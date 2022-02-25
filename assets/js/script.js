cd1e346191fb8c8f0d3f79a8f83bbb9d37a625c4
Dan Kim <danchanyoungkim@gmail.com>
2022-02-24 20:33:51 -0500

    html draft complete

---
+++ assets/js/script.js
@@ -0,0 +1,27 @@
+let questions = [
+    {
+    question: "Which of the following is not a primitive?",
+    choices: ["boolean", "null", "bigint", "element"],
+    answer: "element"
+    },
+    {
+    question: "Which method integrates the text of two strings into one?",
+    choices: ["append()", "add()", "integrate()", "concat()"],
+    answer: "concat()"
+    },
+    {
+    question: "Which of the following words is used to define a variable in JS?",
+    choices: ["var", "let", "Both A and B", "None of the above"],
+    answer: "Both and A and B"
+    },
+    {
+    question: "Upon encountering empty statments, what does the Javascript Interpreter do?",
+    choices: ["Throws an error", "Ignores the statements", "Gives a warning", "None of the above"],
+    answer: "Ignores the statements"
+    },
+    {
+    question: "What does the Javascript 'debugger' statment do?", 
+    choices: ["It will debug all the errors in the program at runtime", "It acts as a breakpoint in a program", "It will debug error in the current statment if any", "All of the above"],
+    answer: "It acts as a breakpoint in a program"
+    },
+    ];
